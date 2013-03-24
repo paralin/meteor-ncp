cbbf0efd74ac613a89ea2fd29ece947ffca5ec2e
kidovate <kidovate@gmail.com>
2013-03-24 14:12:37 -0700

    working, use ncp() not ncp.ncp

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+    summary: "NodeJS's ncp packaged for meteor."
+});
+
+Package.on_use(function (api) {
+    //api.use('jquery', 'client');
+    api.add_files([
+        'lib/ncp.js'
+    ], 'server'
+    );
+});
